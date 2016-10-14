e10dec8fee2e5e27c607ed7e1a928074873b0be2
Virendra <singh.virendra@orangemantra.in>
2016-10-14 08:33:46 +0530

    second commit

--- src/main/java/mscit/Demo.java
+++ src/main/java/mscit/Demo.java
@@ -5,6 +5,8 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("welcome");
+		//welcome to git
+		// this is my second commit
 	}
 
 }
